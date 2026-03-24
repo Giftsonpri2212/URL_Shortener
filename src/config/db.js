bf943bf0f5b4632cc56692948969c8403d152d97
Giftsonpri2212 <giftsonprince602@gmail.com>
2026-03-24 15:42:59 +0530

    added database_url in db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,17 @@
 const { Pool } = require("pg");
 const env = require("./env");
 
+const { Pool } = require("pg");
+
 const pool = new Pool({
-  host: env.postgres.host,
-  port: env.postgres.port,
-  database: env.postgres.database,
-  user: env.postgres.user,
-  password: env.postgres.password,
-  max: env.postgres.max,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 10000
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+module.exports = pool;
+
 
 pool.on("error", (error) => {
   console.error("Unexpected PostgreSQL pool error", error);
